da3b0fae10d1923a42e3d991c70f2504b7c7ee06
FormetcoJS <157385647+FormetcoJS@users.noreply.github.com>
2024-06-20 08:00:11 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(288, 235)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(216, 108)
+        Me.btnSearch.TabIndex = 0
+        Me.btnSearch.Text = "Search List"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSearch As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+
+        Dim astFruit(9) As String
+        Dim stTarget As String
+        Dim bFound As Boolean
+
+        astFruit(0) = "Apple"
+        astFruit(1) = "Mango"
+        astFruit(2) = "Strawberry"
+        astFruit(3) = "Kiwi"
+        astFruit(4) = "Grape"
+        astFruit(5) = "Tomato"
+        astFruit(6) = "Lemon"
+        astFruit(7) = "Melon"
+        astFruit(8) = "Fig"
+        astFruit(9) = "Banana"
+
+        stTarget = InputBox("Which fruit are you looking for?")
+
+        For i = 0 To 9
+            If UCase(astFruit(i)) = UCase(stTarget) Then
+                bFound = True
+                Exit For
+            End If
+        Next
+
+        If bFound = True Then
+            MessageBox.Show("Found it")
+        Else
+            MessageBox.Show("Not Found")
+        End If
+
+    End Sub
+End Class
